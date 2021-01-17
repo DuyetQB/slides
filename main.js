f944338110a8db38fdebe6f2044620f548585c28
DuyetQB <dinhsyduyet@gmail.com>
2021-01-17 19:41:38 +0700

    fix left menu

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ if(window.pageYOffset >= 230 ){
     left.classList.remove("sticky");
     right.style.width = "80%";
     right.style.marginLeft = "0px";
-   footer.style.width = "100%";
-    footer.style.marginLeft = "245px";
+   footer.style.width = "80%";
+    footer.style.marginLeft = "0px";
 }
 }
